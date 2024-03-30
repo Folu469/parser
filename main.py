e88982bb17635e8811ace957e6a634da9ae66f9d
Iliya3129 <qwerty9087650850@gmail.com>
2024-03-31 00:49:11 +0300

    release version 1.0

--- main.py
+++ main.py
@@ -8,26 +8,38 @@ import requests
 
 
 # главная функция
-def parser(url: str):
+def parser(url: str, max_items: int):
     create_csv()
-    list_products = []
-    res = requests.get(url=url)
-    soup = BeautifulSoup(res.text, 'lxml')
 
-    products = soup.find_all('div', class_='product-card')
+    page = 1
+    count_items = 0
 
-    # перебираем каждую карточку товара и собираем данные
-    for product in products:
-        name = product.get('data-product-name')
-        sku = product.get('data-product-sku')
-        price = product.find('span', itemprop='price').get_text()
-        find_href = product.find('meta', itemprop='name')
-        href = find_href.findNext().get('href')
+    # цикл переключает страницы послее каждой итерации
+    while count_items < max_items:
+        list_products = []
+        res = requests.get(url=f'{url}&p={page}')
+        soup = BeautifulSoup(res.text, 'lxml')
 
-        # записываем данные в list_products
-        list_products.append(Product(sku=sku, name=name, price=price, href=href))
+        products = soup.find_all('div', class_='product-card')
 
-    write_scv(list_products)
+        # перебираем каждую карточку товара и собираем данные
+        # цикл лимита карточек
+        for product in products:
+            if count_items >= max_items:
+                break
+            name = product.get('data-product-name')
+            sku = product.get('data-product-sku')
+            price = product.find('span', itemprop='price').get_text()
+            find_href = product.find('meta', itemprop='name')
+            href = find_href.findNext().get('href')
+            count_items += 1
+            print(f'Сейчас на {count_items}стр')
+
+            # записываем данные в list_products
+            list_products.append(Product(sku=sku, name=name, price=price, href=href))
+
+        write_scv(list_products)
+        page += 1
 
 
 # создает csv файл
@@ -46,4 +58,4 @@ def write_scv(products: list[Product]):
 
 
 if __name__ == '__main__':
-    parser(url='https://glavsnab.net/santehnika/rakoviny-i-komplektuyushchiye/rakoviny.html?limit=100')
+    parser(url='https://glavsnab.net/santehnika/rakoviny-i-komplektuyushchiye/rakoviny.html?limit=100', max_items=847)
