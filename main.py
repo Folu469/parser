e380e2a97b250c24c372921a9bc0c46790e2fb48
Iliya3129 <qwerty9087650850@gmail.com>
2024-03-30 20:42:52 +0300

    Create paint parser func

--- main.py
+++ main.py
@@ -5,8 +5,27 @@ from bs4 import BeautifulSoup
 import requests
 
 
+# главная функция
 def parser(url: str):
-    pass
+    res = requests.get(url=url)
+    soup = BeautifulSoup(res.text, 'lxml')
+
+    products = soup.find_all('div', class_='product-card')
+
+    # перебираем каждую карточку товара и собираем данные
+    for product in products:
+        name = product.get('data-product-name')
+        print(name)
+
+        sku = product.get('data-product-sku')
+        print(sku)
+
+        price = product.find('span', itemprop='price').get_text()
+        print(price)
+
+        find_href = product.find('meta', itemprop='name')
+        href = find_href.findNext().get('href')
+        print(href, '\n')
 
 
 def create_csv():
@@ -18,4 +37,4 @@ def write_scv():
 
 
 if __name__ == '__main__':
-    pass
+    parser(url='https://glavsnab.net/santehnika/rakoviny-i-komplektuyushchiye/rakoviny.html?limit=100')
