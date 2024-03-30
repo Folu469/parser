1c04a883ea41ac25e61a77ca9470ecc8c3c5cfea
Iliya3129 <qwerty9087650850@gmail.com>
2024-03-30 18:04:00 +0300

    Basic_project_structure

---
+++ main.py
@@ -0,0 +1,21 @@
+import csv
+
+from bs4 import BeautifulSoup
+
+import requests
+
+
+def parser(url: str):
+    pass
+
+
+def create_csv():
+    pass
+
+
+def write_scv():
+    pass
+
+
+if __name__ == '__main__':
+    pass
