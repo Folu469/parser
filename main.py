5a7f48c600bffa9c2fa4188b06da26258503a3d0
Iliya3129 <qwerty9087650850@gmail.com>
2024-03-30 23:16:50 +0300

    write csv version

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import csv
 
 from bs4 import BeautifulSoup
 
+from model import Product
+
 import requests
 
 
 # главная функция
 def parser(url: str):
+    create_csv()
+    list_products = []
     res = requests.get(url=url)
     soup = BeautifulSoup(res.text, 'lxml')
 
@@ -15,25 +19,30 @@ def parser(url: str):
     # перебираем каждую карточку товара и собираем данные
     for product in products:
         name = product.get('data-product-name')
-        print(name)
-
         sku = product.get('data-product-sku')
-        print(sku)
-
         price = product.find('span', itemprop='price').get_text()
-        print(price)
-
         find_href = product.find('meta', itemprop='name')
         href = find_href.findNext().get('href')
-        print(href, '\n')
+
+        # записываем данные в list_products
+        list_products.append(Product(sku=sku, name=name, price=price, href=href))
+
+    write_scv(list_products)
 
 
+# создает csv файл
 def create_csv():
-    pass
+    with open('glasvsnab.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['sku', 'name', 'price', 'href'])
 
 
-def write_scv():
-    pass
+# записывает данные в csv файл
+def write_scv(products: list[Product]):
+    with open('glasvsnab.csv', mode='a', newline='') as file:
+        writer = csv.writer(file)
+        for product in products:
+            writer.writerow([product.sku, product.name, product.price, product.href])
 
 
 if __name__ == '__main__':
